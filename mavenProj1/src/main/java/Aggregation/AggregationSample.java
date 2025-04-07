5d2e6db3c44ccba476423444822bb6a8062a61fb
USER <USER@DESKTOP-U873TNQ>
2025-04-07 10:04:03 +0400

    NewAggregation

---
+++ mavenProj1/src/main/java/Aggregation/AggregationSample.java
@@ -0,0 +1,14 @@
+package Aggregation;
+
+public class AggregationSample {
+	String name;
+	int rollno;
+	String address;
+	
+	public AggregationSample(String name, int rollno, String address){
+		this.name=name;
+		this.rollno=rollno;
+		this.address=address;
+	}
+
+}
